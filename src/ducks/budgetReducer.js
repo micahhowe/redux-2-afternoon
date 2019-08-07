8a2b0f82477d066d2055a76d344bc474b2d98ad0
micahhowe <micahhowe182@gmail.com>
2019-08-07 14:32:43 -0600

    redux store is getting set up

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+  }
+
+
+  export default function reducer(state = initialState, action) {
+    // const {type, payload} = action
+    // switch(type) {
+    //     default: 
+        return state
+    }
