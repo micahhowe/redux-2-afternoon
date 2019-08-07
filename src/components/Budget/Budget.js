0e1cbdd93cf64473ac9e6eb871e90ef7368941e1
micahhowe <micahhowe182@gmail.com>
2019-08-07 16:09:32 -0600

    add and remove are now working correctly

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -9,14 +9,16 @@ import Nav from './../shared/Nav';
 import './Budget.css';
 import {connect} from 'react-redux'
 import { requestUserData } from './../../ducks/userReducer'
+import { requestBudgetData, addPurchase, removePurchase } from './../../ducks/budgetReducer'
 
 
 class Budget extends Component {
   componentDidMount(){
     this.props.requestUserData()
+    this.props.requestBudgetData()
   }
   render() {
-    const { loading } = this.props.budget
+    const { loading, purchases, budgetLimit } = this.props.budget
     const { firstName, lastName} = this.props.user
     return (
       <Background>
@@ -26,12 +28,12 @@ class Budget extends Component {
           <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase}/>
+              <DisplayPurchases purchases={purchases} removePurchase={this.props.removePurchase}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -46,5 +48,5 @@ function mapStateToProps(state) {
     user: state.user
   }
 }
-
-export default connect(mapStateToProps, { requestUserData })(Budget);
+//Don't forget The second arg to the connect method is an object that you can put any needed action creators in
+export default connect(mapStateToProps, { requestUserData, requestBudgetData, addPurchase, removePurchase })(Budget);
